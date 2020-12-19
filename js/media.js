369d5502eb31b208792611ac2f3cf7c1b84a4a55
kbtony <tony790511@yahoo.com.tw>
2020-12-20 02:35:44 +1100

    Responsive Topnav

---
+++ js/media.js
@@ -0,0 +1,21 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+/*
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }*/
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+  
