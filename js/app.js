3f71a078a0f6ab1679ff8abe54365f4dbc9b8476
kbtony <tony790511@yahoo.com.tw>
2020-12-22 14:29:50 +1100

    project structure

--- js/app.js
+++ js/app.js
@@ -4,9 +4,6 @@ const slider = document.querySelector(".slider");
 const topnav = document.querySelector(".topnav");
 const headline = document.querySelector(".headline");
 
-
-
-
 const tl = new TimelineMax();
 
 tl.fromTo(hero, 1, {height: "0%"}, {height:"100%", ease: Power2.easeInOut })
