517fd111afef886c4cb8caf3f5294417333a6925
kbtony <tony790511@yahoo.com.tw>
2021-02-15 20:30:32 +1100

    move myFunction2 to app.js

--- js/app.js
+++ js/app.js
@@ -4,14 +4,22 @@ const slider = document.querySelector(".slider");
 const topnav = document.querySelector(".topnav");
 const headline = document.querySelector(".headline");
 
-// Test Animated Responsive Hamburger Navbar
+const tl = new TimelineMax();
+
+tl.fromTo(hero, 1, {height: "0%"}, {height:"100%", ease: Power2.easeInOut })
+.fromTo(hero, 1.2, {width: "100%"}, {width:"85%", ease: Power2.easeInOut })
+.fromTo(slider, 1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut }, "-=1.2")
+.fromTo(topnav, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5")
+//.fromTo(headline, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5")
+
+
+// Animated Responsive Hamburger Navbar
 const hamburger = document.querySelector(".icon");
 const navLinks = document.querySelector(".hamburger-links");
 const links = document.querySelector(".hamburger-links li");
 
 const ham2 = document.querySelector(".hamburger-links li #ham2");
 const ham3 = document.querySelector(".hamburger-links li #ham3");
-
 
 
 hamburger.addEventListener("click", () => {
@@ -32,16 +40,19 @@ ham2.addEventListener("click", () => {
 ham3.addEventListener("click", () => {
     navLinks.classList.toggle("open");
 });
-
-// Test Animated Responsive Hamburger Navbar
-
+// Animated Responsive Hamburger Navbar
 
 
-const tl = new TimelineMax();
+// Sticky Navbar
+window.onscroll = function() {myFunction2()};
+        
+var navbar = document.getElementById("myTopnav");
+var sticky = navbar.offsetTop;
 
-tl.fromTo(hero, 1, {height: "0%"}, {height:"100%", ease: Power2.easeInOut })
-.fromTo(hero, 1.2, {width: "100%"}, {width:"85%", ease: Power2.easeInOut })
-.fromTo(slider, 1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut }, "-=1.2")
-.fromTo(topnav, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5")
-//.fromTo(headline, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5")
-
+function myFunction2() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
